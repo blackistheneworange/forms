f0fc45bc09b14e27dfd2d2fd3396455a3d512990
Hari <hharan87@gmail.com>
2020-05-23 22:13:33 +0530

    imgPath15

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -261,7 +261,7 @@ router.route('/upload')
      
      
      
-     res.send({fileName:'../public/images/'+req.file.filename});
+     res.send({fileName:'images/'+req.file.filename});
      
    })
    
