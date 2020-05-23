c081b3d042af4db1d0199513bc9234e03ccec251
Hari <hharan87@gmail.com>
2020-05-23 21:59:25 +0530

    imgPath8

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express=require('express');
 var bodyParser=require('body-parser');
 var morgan=require('morgan');
 var path=require('path');
-process.env.PWD=process.cwd();
+
 var mongoose=require('mongoose');
 var debug=require('debug');
 var cookieParser=require('cookie-parser');
@@ -35,9 +35,9 @@ var auth=require('./routes/authRouter');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-//app.use(express.static(path.join(process.env.PWD,'public')));
-//app.use('/images',express.static('public/images'))
-//app.use(upload.array());
+app.use(express.static('public');
+
+
 app.use(session({secret:'test',resave:true,saveUninitialized:false}))
 app.use(cookieParser());
 app.use(morgan('dev'));
